949bfb63036a0ae2a070da97b5c0f0c99724d8c6
Ethan Ochoa <ethanochoa@Ethans-MacBook-Pro.local>
2022-08-24 21:31:27 -0400

    first commit - connected to DB and React

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return(
+        <ul>
+            <li><a href="/"></a>Home</li>
+            <li><a href="/add-product">Add Products</a></li>
+            <li><a href="/delete-product">Delete Products</a></li>
+        </ul>
+    )
+}
+export default Header
---
+++ src/pages/home.js
@@ -0,0 +1,25 @@
+import {useState} from 'react'
+
+
+
+const Home = () => {
+const [products, setProducts] = useState([])
+
+useEffect(() => {
+    fetch('https://localhost:4040')
+    .then(res => res.json())
+    .then (data => setProducts(data))
+    .catch (err => console.error(err))
+}, [])
+
+const allProducts = products.map(product => <li>{product.name}</li>)
+
+    return (
+        <>
+        <h1>Home component</h1>
+        <ul>{allProducts}</ul>
+        </>
+    )
+}
+
+export default Home
